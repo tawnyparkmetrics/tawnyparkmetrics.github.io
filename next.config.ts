f249d39597689240437eb664d7c7a819bdf39438
Seena Pourzand <seenapourzand@gmail.com>
2025-01-25 22:10:22 -0800

    revert unecessary changes

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
-  },
-  output: 'export',
-  // If you have any static pages that require dynamic data,
-  // you might need to specify them here
-  trailingSlash: true,
-  
+  }
 };
 
 export default nextConfig;
