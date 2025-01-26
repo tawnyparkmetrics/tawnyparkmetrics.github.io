a7f281484efc9f3da1515423d5d6031e0248cb81
Seena Pourzand <seenapourzand@gmail.com>
2025-01-25 22:06:52 -0800

    bug fix 2 for deploy

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
-  }
+  },
+  output: 'export',
+  // If you have any static pages that require dynamic data,
+  // you might need to specify them here
+  trailingSlash: true,
+  
 };
 
 export default nextConfig;
