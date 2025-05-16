dc06613b96e2b7b19c48aa8369460f7bdb975cfa
Bala Ravikumar <bala.ravi2002@gmail.com>
2025-05-15 22:59:39 -0300

    sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://tawnyparkmetrics.com',
+    generateRobotsTxt: true,
+    // Optional: add any custom configurations
+  }
