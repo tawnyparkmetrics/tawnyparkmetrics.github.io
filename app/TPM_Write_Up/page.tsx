c027d3bf3ee6bfe8dbb68df8d59cbfcb075c4499
Bala Ravikumar <bala.ravi2002@gmail.com>
2025-05-24 14:44:09 -0300

    remove link

--- app/TPM_Write_Up/page.tsx
+++ app/TPM_Write_Up/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from 'react';
-import Link from 'next/link';
 import { ChevronRight, ChevronDown } from 'lucide-react';
 import NavigationHeader from '@/components/NavigationHeader';
 
