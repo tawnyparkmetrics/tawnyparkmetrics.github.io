344b38cc38608663e0007788277cda12f2f713d5
Bala Ravikumar <bala.ravi2002@gmail.com>
2025-07-11 20:01:26 -0300

    404 fix

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirects = {
+      '/Nick_Draft_Page': '/nick-nba-draft-board',
+      '/Andre_Draft_Page': '/andre-nba-draft-board',
+      '/TPM_Draft_Page': '/max-nba-draft-board',
+      '/TPM_Write_Up': '/max-nba-draft-model-write-up',
+      '/Consensus': '/consensus-nba-draft-board',
+    };
+
+    if (redirects[router.asPath]) {
+      router.replace(redirects[router.asPath]);
+    }
+  }, [router]);
+
+  return <div>Redirecting...</div>;
+}
